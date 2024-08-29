a9fae68235f355c9754048150ecd66c974d1674e
csheha <chanudishehani22@gmail.com>
2024-08-29 18:22:28 +0530

    express server MongoDB Connection done

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+const app = express();
+dotenv.config();
 
+//connecting the database 
+const connect = async () =>{
+    try 
+    {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB!");
+    } 
+    catch (error) 
+    {
+        console.log(error);
+    }
+}
 app.listen(8800, ()=>
 {
+    connect();
     console.log("Backend server is running!")
 })
